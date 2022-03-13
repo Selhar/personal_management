7e0192f8fe69226ffb04b60160420dd975dc195c
selhar <selhar@protonmail.com>
2022-03-12 21:07:00 -0300

    feat: boilerplate

---
+++ README.md
@@ -0,0 +1,4 @@
+To-do list:
+
+- Write a proper readme
+- Fix pathing on vscode (or set up vim)
---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Card from "@compon"
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+</style>
